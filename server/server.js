675f18be7dd8b268c924f3891505d96ca98fe100
Pavalan G <136297958+Pavalanganesan2368@users.noreply.github.com>
2026-01-30 13:49:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',') : "http://localhost:5173",
+     origin: "*",
     credentials: true,
   }),
 );
